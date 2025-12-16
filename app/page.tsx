edc948dede1e292841108e5a3a03d33a97e311a3
Laura Ogbechie <ogbechielaura04@gmail.com>
2025-12-16 09:30:25 +0100

    resolved height issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import CryptoConvert from "./crypto-convert/page";
 
 export default function Home() {
   return (
-    <div className="bg-gray-200 h-dvh px-4">
-      <main className="flex h-full items-center">
+    <div className="bg-gray-200 min-h-dvh px-4">
+      <main className="flex h-full justify-center">
         <CryptoConvert />
       </main>
     </div>
